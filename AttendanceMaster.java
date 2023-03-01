9a585ee2c7323e8ff9507d334e52cb5e4694e6a8
kaushikbalajims-toast <kaushik.balaji.m.s@toasttab.com>
2023-03-01 12:54:26 +0530

    Attendance master update

--- AttendanceMaster.java
+++ AttendanceMaster.java
@@ -11,11 +11,29 @@ public class AttendanceMaster {
     }
 
     public void showEligibleList(){
+        int eligibleCount = 0;
         System.out.println("---------- Eligible employees ----------\n");
         ArrayList<Employee> emList = new ArrayList<Employee>(this.empAtten.keySet()) ;
+        System.out.format("%10s %20s %20s %20s %20s", "Employee ID", "Name", "Designation", "Department", "Salary");
+        System.out.println();
+        System.out.println("-------------------------------------------------------------------------------------------------");
         for (Employee emp : emList) {
             if(this.empAtten.get(emp) > 9){
                 System.out.println(emp.toString());
+                eligibleCount++;
+            }
+        }
+
+        if(eligibleCount == 0){
+            System.out.println("No employees eligible\n");
+        }
+    }
+
+    public void FilterEmployeeList(){
+        ArrayList<Employee> emList = new ArrayList<Employee>(empAtten.keySet());
+        for (Employee emp : emList) {
+            if(empAtten.get(emp) < 10){
+                empAtten.remove(emp);
             }
         }
     }
